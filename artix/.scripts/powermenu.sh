c006f258745cdc63dd108fc0713d1fff2054f155
Muhammad Rafiuddin <71545866+rafimrfdn@users.noreply.github.com>
2021-12-10 20:36:37 +0800

    update powermenu launch in rofi

--- artix/.scripts/powermenu.sh
+++ artix/.scripts/powermenu.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 function powermenu {
-    options="Cancel\nSleep\nRestart\nShutdown"
-    selected=$(echo -e $options | dmenu)
+    options="Cancel\nLogout\nSleep\nRestart\nShutdown"
+    selected=$(echo -e $options | rofi -dmenu)
     if [[ $selected = "Shutdown" ]]; then
         loginctl poweroff
     elif [[ $selected = "Restart" ]]; then
         loginctl reboot
     elif [[ $selected = "Sleep" ]]; then
         loginctl suspend
+    elif [[ $selected = "Logout" ]]; then
+       pkill -KILL -u $USER 
     elif [[ $selected = "Cancel" ]]; then
         return
     fi
