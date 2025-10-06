98460c820643f307ce3f1f8a60ee9cde14850ea4
rafimrfdn <uphy.mks@gmail.com>
2025-10-06 09:04:07 +0800

    fitst init

---
+++ .config/fish/conf.d/deno.fish
@@ -0,0 +1,1 @@
+source "/home/cachy/.deno/env.fish"
---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+# function fish_greeting
+#      smth smth
+# end
+
+alias neofetch="fastfetch"
+alias vim="nvim"
