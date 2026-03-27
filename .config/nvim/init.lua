6cb01c6c6335292917cd6cb5be39eda5ef0d3a9d
rafimrfdn <uphy.mks@gmail.com>
2026-03-28 05:08:03 +0800

    refined .config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,5 +4,10 @@ vim.g.maplocalleader = " "
 require("config")
 require("plugins")
 
+-- enable the LSP since we have installed them from Mason
 -- vim.lsp.enable({ "lua_ls", "javascript", "emmet_ls", "emmet_language_server", "html" })
 vim.lsp.enable({ "lua_ls", "javascript", "ts_ls", "html" })
+
+-- I want to use the colorscheme from terminal
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
